62334ccf9a267822aea4f3d5d62b9a79f809855a
Sarah D'Agostino <166957570+dagosaurusrex@users.noreply.github.com>
2024-09-03 14:56:23 -0700

    Add variables for user wins, comp wins and current round

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 console.info("This game is similar to Rock, Paper, Scissors. You choose one object and the computer will choose another, at random. After 5 rounds, a winner (or a tie game) will be declared! GLHF")
+
+let currentRound=1;
+let userWins=0;
+let compWins=0;
