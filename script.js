eab1a2bfa99d42aa7aced6c58b97120c19b3f827
Sarah D'Agostino <166957570+dagosaurusrex@users.noreply.github.com>
2024-09-03 20:44:11 -0700

    Revise JS reference and add button event listeners

--- script.js
+++ script.js
@@ -3,3 +3,27 @@ console.info("This game is similar to Rock, Paper, Scissors. You choose one obje
 let currentRound = 1;
 let userWins=0;
 let compWins=0;
+
+
+/*Button event listeners and button function*/
+document.getElementById('index').addEventListener('click', function() {
+    handleButtonClick("INDEX");
+});
+
+document.getElementById('table').addEventListener('click', function() {
+    handleButtonClick("TABLE");
+});
+
+document.getElementById('query').addEventListener('click', function() {
+    handleButtonClick("QUERY");
+});
+
+function handleButtonClick(buttonValue) {
+    let myVariable = buttonValue;
+    console.log('Button clicked ', myVariable);
+    runMyFunction(myVariable); 
+}
+
+function runMyFunction(value) {
+    alert("You clicked " + value);
+}
