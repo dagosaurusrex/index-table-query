55a695353294c0018c8b974975a227dd7c29a730
Sarah D'Agostino <166957570+dagosaurusrex@users.noreply.github.com>
2024-09-03 21:12:58 -0700

    Create startRound function

--- script.js
+++ script.js
@@ -8,14 +8,17 @@ let compWins=0;
 /*Button event listeners and button function*/
 document.getElementById('index').addEventListener('click', function() {
     handleButtonClick("INDEX");
+    startRound("INDEX");
 });
 
 document.getElementById('table').addEventListener('click', function() {
     handleButtonClick("TABLE");
+    startRound("TABLE");
 });
 
 document.getElementById('query').addEventListener('click', function() {
     handleButtonClick("QUERY");
+    startRound("QUERY");
 });
 
 function handleButtonClick(buttonValue) {
@@ -26,4 +29,20 @@ function handleButtonClick(buttonValue) {
 
 function runMyFunction(value) {
     alert("You clicked " + value);
+}
+
+function startRound(buttonValue) {
+    let compSelection = 'TABLE';
+    if (compSelection == buttonValue) {
+        alert("Tie!");
+        /*advanceRound();  I'll add this back in after I create the advanceRound function*/
+    } else if ((buttonValue == 'TABLE' && compSelection == 'QUERY')
+    || (buttonValue == 'QUERY' && compSelection == 'INDEX')
+    || (buttonValue == 'INDEX' && compSelection == 'TABLE')) {
+        alert("You won this round!");
+        /*roundWon();  I'll add this back in after I create the roundWon() function */
+    } else {
+        alert("Oop! I won this round. Better luck next time!");
+        /*roundLost();  I'll add this back in after I create the roundLost() function*/
+    }
 }
